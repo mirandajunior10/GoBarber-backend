a4b601188bfa56a2f174528e310ba1b12989f066
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-15 00:40:01 -0300

    Arquitetura e testes no Node.JS: Módulo 1 - Configurando Imports

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,10 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+
+import '@shared/infra/typeorm';
 import routes from './routes';
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
 const app = express();
 app.use(cors());
