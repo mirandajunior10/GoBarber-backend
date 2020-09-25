e08151689742735fc192fa9e2df9c4435fc44e2d
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-25 00:12:42 -0300

    Continuando back-end do app: Módulo 3 - Filtrando agendamentos por mês

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'tsyringe';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointmentsInMonth = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+      },
+    );
+
+    console.log(appointmentsInMonth);
+    return [{ day: 1, available: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
