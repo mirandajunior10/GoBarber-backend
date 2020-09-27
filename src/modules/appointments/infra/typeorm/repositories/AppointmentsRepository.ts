021f471375e548597e46fb20ed29344db6379c0b
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-26 23:50:23 -0300

    Continuando back-end do app: Módulo 3 - Rotas e controllers

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -47,7 +47,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_char(${dateFieldName}, 'MM-YYYY') = ${parsedMonth}-${year} `,
+            `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}' `,
         ),
       },
     });
@@ -69,7 +69,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_char(${dateFieldName}, 'DD-MM-YYYY') = ${parsedDay}-${parsedMonth}-${year} `,
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}' `,
         ),
       },
     });
