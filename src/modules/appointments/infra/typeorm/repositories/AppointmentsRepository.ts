516621cae0da63ca41242df2eb03c8071a7c8732
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-12-19 16:49:01 -0300

    Finzalizando front-end web: Módulo 1 - Dias indisponíveis no mês

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -75,6 +75,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}' `,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
