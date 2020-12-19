5ab5ae27b554d76915236ac77fea8700e98d2f24
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-12-19 17:39:16 -0300

    Finzalizando front-end web: Módulo 1 - Serialização no cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import { inject, injectable } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -36,7 +37,7 @@ class ListProviderAppointmentsService {
         { day, month, year, provider_id },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
