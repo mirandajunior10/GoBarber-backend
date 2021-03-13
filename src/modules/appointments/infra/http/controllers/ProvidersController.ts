046ba4c3652466d699d5ce10563024ad4d3bd356
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2021-03-13 12:28:11 -0300

    Finalizando : Finalizando front-end do mobile: Módulo 1 - Lista de providers com serialização

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
   public async index(req: Request, res: Response): Promise<Response> {
@@ -13,6 +14,6 @@ export default class ProvidersController {
       user_id,
     });
 
-    return res.json(providers);
+    return res.json(classToClass(providers));
   }
 }
